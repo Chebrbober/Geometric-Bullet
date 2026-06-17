16e9dd8da1cec6ffb1628bf0c578330a41f97682
beaver <meyar2223@gmail.com>
2026-06-17 10:52:25 +0300

    Working pathfinding

--- entities/enemies/cube_enemy/cube_enemy.gd
+++ entities/enemies/cube_enemy/cube_enemy.gd
@@ -1,9 +1,18 @@
 class_name CubeEnemy
 extends Entity
 
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+
 func _physics_process(delta: float) -> void:
-    if NodeSpawner.player != null:
-        var direction = global_position.direction_to(NodeSpawner.player.global_position)
-        velocity = direction * speed
-    
-    move_and_collide(velocity * delta)
+	var direction = to_local(nav_agent.get_next_path_position()).normalized()
+	if NodeSpawner.player != null:
+		velocity = direction * speed
+
+	move_and_slide()
+
+func makepath() -> void:
+	if NodeSpawner.player != null:
+		nav_agent.target_position = NodeSpawner.player.global_position
+
+func _on_timer_timeout() -> void:
+	makepath()
