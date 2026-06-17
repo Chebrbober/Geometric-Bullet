fec125d37ce3dee781bf84dcc9dd473d2e67b600
beaver <meyar2223@gmail.com>
2026-06-17 10:29:35 +0300

    Fix collision issues

--- entities/enemies/cube_enemy/cube_enemy.gd
+++ entities/enemies/cube_enemy/cube_enemy.gd
@@ -2,7 +2,8 @@ class_name CubeEnemy
 extends Entity
 
 func _physics_process(delta: float) -> void:
-	if NodeSpawner.player != null:
-		velocity = global_position.direction_to(NodeSpawner.player.global_position)
-	global_position += velocity * speed * delta
-	move_and_slide()
+    if NodeSpawner.player != null:
+        var direction = global_position.direction_to(NodeSpawner.player.global_position)
+        velocity = direction * speed
+    
+    move_and_collide(velocity * delta)
